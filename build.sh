9a702a789530e20c926a454adb8c35371ec85868
tranphuquy19 <tranphuquy19@gmail.com>
2021-04-10 02:35:03 +0700

    update gitignore && build.sh

--- build.sh
+++ build.sh
@@ -37,12 +37,15 @@ while read line; do
     output_path="$WORKING_DIR/build/$GOOS/$GOARCH/$output_name"
 
     echo "Building for OS=$GOOS Architecture=$GOARCH"
-    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_path
+
+    GOOS=$GOOS GOARCH=$GOARCH go build -o $output_path
+
     if [ ! -f $output_path ]; then
         echo "Failed when build for OS=$GOOS Architecture=$GOARCH"
         exit 1
     else
-        echo "Done with output file: $output_path"
+        fileSize=$(find "$fileName" -printf "%s")
+        echo "Done with output file: $output_path ($fileSize bytes)"
     fi
 
     if [ $? -ne 0 ]; then
