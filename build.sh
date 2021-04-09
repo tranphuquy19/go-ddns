5e3d3108a6f7a0ef4434078a10cdd847c25172e2
tranphuquy19 <tranphuquy19@gmail.com>
2021-04-10 01:13:41 +0700

    update gitignore & platforms file

--- build.sh
+++ build.sh
@@ -7,20 +7,22 @@ FULL_PATH_PLATFORMS_FILE=$WORKING_DIR/$PLATFORMS_FILE
 APP_NAME=go-ddns
 
 # check file platforms.txt
-if [ -e $FULL_PATH_PLATFORMS_FILE ]; then
+if [ ! -f $FULL_PATH_PLATFORMS_FILE ]; then
     echo "Platforms file: $FULL_PATH_PLATFORMS_FILE does not exist"
     exit 1
 fi
 
 # remove build folder
+echo "Clean build folder"
 rm -rf ./build
 
 # create build folder
+echo "Create build folder"
 mkdir -p build
 
 # read platforms file
 while read line; do
-    temp=(${line// / })
+    temp=(${line//\// })
 
     GOOS=${temp[0]}
     GOARCH=${temp[1]}
@@ -33,10 +35,13 @@ while read line; do
 
     mkdir -p build/$GOOS/$GOARCH
 
-    env GOOS=$GOOS GOARCH=$GOARCH go build -o $WORKING_DIR/build/$output_name
+    echo "Building for OS=$GOOS PLATFORM=$GOARCH"
+    env GOOS=$GOOS GOARCH=$GOARCH go build -o $WORKING_DIR/build/$GOOS/$GOARCH/$output_name
 
     if [ $? -ne 0 ]; then
         echo 'An error has occurred! Aborting the script execution...'
         exit 1
     fi
 done < $PLATFORMS_FILE
+
+echo "DONE"
