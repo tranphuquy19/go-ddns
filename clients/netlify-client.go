22c9e005f31ef6d79fef5d784a942979675d443b
Quy (Christian) P. TRAN <christian@enclave.vn>
2021-04-23 09:13:36 +0700

    improve performance

--- clients/netlify-client.go
+++ clients/netlify-client.go
@@ -33,19 +33,25 @@ func GetRecordById(zoneId string, recordId string, record *parser.Record) gjson.
 func NetlifyUpdateRecord(domainName string, record *parser.Record, token string) {
 	netlifyClient = InitNetlifyClient(token)
 	url := util.ParseRecordURL(record.Name, domainName)
-	fmt.Println(url)
+	// fmt.Println(url)
 	zones := GetDNSZones().Array()
+	zoneId := ""
 	for _, _zone := range zones {
 		jZone := gjson.Parse(_zone.Raw)
-		zoneName, zoneId := jZone.Get("name").String(), jZone.Get("id").String()
+		zoneName, _zoneId := jZone.Get("name").String(), jZone.Get("id").String()
 		if zoneName == domainName {
-			records := GetRecords(zoneId).Array()
-			for _, _record := range records {
-				jRecord := gjson.Parse(_record.Raw)
-				recordName, recordId, recordType, recordValue := jRecord.Get("hostname").String(), jRecord.Get("id").String(), jRecord.Get("type").String(), jRecord.Get("value").String()
-				fmt.Println(recordName, recordId, recordType, recordValue)
-			}
+			zoneId = _zoneId
 			break
 		}
+	}
+	if zoneId != "" {
+		records := GetRecords(zoneId).Array()
+		for _, _record := range records {
+			jRecord := gjson.Parse(_record.Raw)
+			recordName, recordId, recordType, recordValue := jRecord.Get("hostname").String(), jRecord.Get("id").String(), jRecord.Get("type").String(), jRecord.Get("value").String()
+			fmt.Println(recordName, recordId, recordType, recordValue)
+		}
+	} else {
+		util.HandleError(nil, fmt.Sprintf("Site not found: %s", url))
 	}
 }
